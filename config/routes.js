859050481a4846c18b7e984a734b6bf89b13c0c5
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-05-09 15:22:23 -0600

    Added express and controllers in routes.js

---
+++ config/routes.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var router = express.Router();
+var userController = require('../controllers/users.js');
+var adminController = require('../controllers/admin.js');
