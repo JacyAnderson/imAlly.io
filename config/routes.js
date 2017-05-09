ce91ed10426be3fdc0d0af371adecc7bad647efd
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-05-09 16:40:46 -0600

    Add adminController routes in config/routes.js

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var resourceController = require('../controllers/resources.js');
 var adminController = require('../controllers/admin.js');
 
-//Resource Routes
+// Resource Routes
 
 // index
 router.get('/api/resources', resourceController.index);
@@ -19,5 +19,22 @@ router.put('/api/resources/:id', resourceController.update);
 
 // destroy 
 router.delete('/api/resources/:id', resourceController.destroy);
+
+// Admin Routes 
+
+// index 
+router.get('/api/admin', adminController.index);
+
+// create
+router.post('/api/admin', adminController.create);
+
+// show
+router.get('/api/admin/:id', adminController.show);
+
+// update
+router.put('/api/admin/:id', adminController.update);
+
+// destroy
+router.delete('/api/admin/:id' , adminController.destroy);
 
 module.exports = router;
