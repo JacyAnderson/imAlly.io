fa44180e4461879e8e6176470a9a3f5b33de4d5c
Taylor Laine <tlaine@msudenver.edu>
2017-05-11 11:11:43 -0600

    fixes bootstrap issues

---
+++ public/scripts/services/account.js
@@ -0,0 +1,104 @@
+angular
+  .service('Account', Account);
+
+  
+//////////////
+// Services //
+//////////////
+
+Account.$inject = ["$http", "$q", "$auth"]; // minification protection
+function Account($http, $q, $auth) {
+  var self = this;
+  self.user = null;
+
+  self.signup = signup;
+  self.login = login;
+  self.logout = logout;
+  self.currentUser = currentUser;
+  self.getProfile = getProfile;
+  self.updateProfile = updateProfile;
+  self.loginAttempt = false;
+
+  function signup(userData) {
+    return (
+      $auth
+        .signup(userData) // signup (https://github.com/sahat/satellizer#authsignupuser-options)
+        .then(
+          function onSuccess(response) {
+            $auth.setToken(response.data.token); // set token (https://github.com/sahat/satellizer#authsettokentoken)
+          },
+
+          function onError(error) {
+            console.error(error);
+          }
+        )
+    );
+  }
+
+  function login(userData) {
+    return (
+      $auth
+        .login(userData) // login (https://github.com/sahat/satellizer#authloginuser-options)
+        .then(
+          function onSuccess(response) {
+            $auth.setToken(response.data.token); // set token (https://github.com/sahat/satellizer#authsettokentoken)
+          },
+
+          function onError(error) {
+            console.error(error);
+            self.loginFail = true;
+          }
+        )
+    );
+  }
+
+  function logout() {
+    return (
+      $auth
+        .logout() // delete token (https://github.com/sahat/satellizer#authlogout)
+        .then(function() {
+          self.user = null;
+        })
+    );
+  }
+
+  function currentUser() {
+    if ( self.user ) { return self.user; }
+    if ( !$auth.isAuthenticated() ) { return null; }
+
+    var deferred = $q.defer();
+    getProfile().then(
+      function onSuccess(response) {
+        self.user = response.data;
+        deferred.resolve(self.user);
+      },
+
+      function onError() {
+        $auth.logout();
+        self.user = null;
+        deferred.reject();
+      }
+    )
+    self.user = promise = deferred.promise;
+    return promise;
+
+  }
+
+  function getProfile() {
+    return $http.get('/api/me');
+  }
+
+  function updateProfile(profileData) {
+    return (
+      $http
+        .put('/api/me', profileData)
+        .then(
+          function (response) {
+            self.user = response.data;
+          }
+        )
+    );
+  }
+
+
+}
