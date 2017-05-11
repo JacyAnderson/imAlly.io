5dfa798752afe341e154bd75c96c946342fbf0eb
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-05-11 16:34:49 -0600

    Transfer user functions from server to userController

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,51 @@ var User = db.models.User;
 var auth = require('../resources/auth');
 
 
+// function index (req, res) {
+//   User.findById(req.user).then(function (user) {
+//     if (!user) {
+//       return res.status(400).send({ message: 'User not found.' });
+//     }
+//     res.send(user);
+//   });
+// }
+
+// function update (req, res) {
+//   User.findById(req.user).then(function (user) {
+//     if (!user) {
+//       return res.status(400).send({ message: 'User not found.' });
+//     }
+//     user.displayName = req.body.displayName || user.displayName;
+//     user.username = req.body.username || user.username;
+//     user.email = req.body.email || user.email;
+//     user.save().then(function(result) {
+//       if (!result) {
+//         res.status(500).send({ message: "Oh noes an error!" });
+//       }      
+//       res.send(result);
+//     });
+//   });
+// }
+
+function create (req, res) {
+  User.findOne({where: { email: req.body.email }}).then(function (existingUser) {
+    if (existingUser) {
+      return res.status(409).send({ message: 'Email is already taken.' });
+    }
+    var user = User.build({
+      displayName: req.body.displayName,
+      email: req.body.email,
+      password: req.body.password
+    });
+    user.save().then(function (result) {
+      if (!result) {
+        res.status(500).send({ message: "Oh noes an error!" });
+      }
+      res.send({ token: auth.createJWT(result) });
+    });
+  });
+}
+
 function login (req, res) {
   User.findOne({where: { email: req.body.email }}).then(function (existingUser) {
     if (!existingUser) {
@@ -17,5 +62,8 @@ function login (req, res) {
 }
 
 module.exports = {
+  index: index,
+  update: update,
+  create: create,
   login: login
-}
+};
