a6ccb77fcc56001cd9c4007f9c9a2019c1379db8
Taylor Laine <tlaine@msudenver.edu>
2017-05-14 20:14:08 -0600

    connect info for heroku sql

--- models/index.js
+++ models/index.js
@@ -2,7 +2,18 @@
 var Sequelize = require('sequelize');
 require('dotenv').config();
 
-var sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
+}
+//var sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
 //kj
 
 //Export models and Sequelize for seed and dbSetup
