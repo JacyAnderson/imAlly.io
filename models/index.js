208adb803b2577924549bdec0dff83c0aa05b0f2
Taylor Laine <tlaine@msudenver.edu>
2017-05-14 20:26:06 -0600

    try to connect postgress heroku

--- models/index.js
+++ models/index.js
@@ -2,19 +2,31 @@
 var Sequelize = require('sequelize');
 require('dotenv').config();
 
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    logging:  true //false
-  });
-} else {
-  // the application is executed on the local machine
-  sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
-}
+// if (process.env.DATABASE_URL) {
+//   // the application is executed on Heroku ... use the postgres database
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:  'postgres',
+//     protocol: 'postgres',
+//     logging:  true //false
+//   });
+// } else {
+//   // the application is executed on the local machine
+//   sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
+// }
 //var sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
 //kj
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 //Export models and Sequelize for seed and dbSetup
 module.exports.Sequelize = Sequelize;
