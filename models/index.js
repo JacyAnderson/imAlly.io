b8cb2780ee1c46cdd4fa4e25e19c309051289b2c
Taylor Laine <tlaine@msudenver.edu>
2017-05-14 20:33:35 -0600

    heroku

--- models/index.js
+++ models/index.js
@@ -7,14 +7,16 @@ require('dotenv').config();
 if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
   sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
+    dialect: 'postgres',
     protocol: 'postgres',
-    logging:  true //false
-  });
+    dialectOptions: {
+        ssl: true
+    }});
 } else {
   // the application is executed on the local machine
   sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
 }
+
 //var sequelize = new Sequelize('postgres://' + process.env.USERNAME + '@localhost:5432/ally');
 //kj
 
